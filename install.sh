fb377f62313c35f5bc2992b48a38fedda6d336d5
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-07-13 05:51:34 +0600

    Update install.sh
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
+usageFunction()
+{
+  echo " "
+  tput setaf 2;
+  echo "Usage: $0 (-n) (-h)"
+  echo -e "\t-n Non-interactive installation (Optional)"
+  echo -e "\t-h Show usage"
+  exit 1
+}
+
 tput setaf 2;
-cat web/art/reconPoint.txt
+cat web/art/reNgine.txt
 
 tput setaf 1; echo "Before running this script, please make sure Docker is running and you have made changes to .env file."
 tput setaf 2; echo "Changing the postgres username & password from .env is highly recommended."
 
 tput setaf 4;
-read -p "Are you sure, you made changes to .env file (y/n)? " answer
-case ${answer:0:1} in
-    y|Y|yes|YES|Yes )
-      echo "Continiuing Installation!"
-    ;;
-    * )
-      if [ -x "$(command -v nano)" ]; then
-        tput setaf 2; echo "nano already installed, skipping."
-      else
-        sudo apt update && sudo apt install nano -y
-        tput setaf 2; echo "nano installed!!!"
-      fi
-    nano .env
-    ;;
-esac
+
+isNonInteractive=false
+while getopts nh opt; do
+   case $opt in
+      n) isNonInteractive=true ;;
+      h) usageFunction ;;
+      ?) usageFunction ;;
+   esac
+done
+
+if [ $isNonInteractive = false ]; then
+    read -p "Are you sure, you made changes to .env file (y/n)? " answer
+    case ${answer:0:1} in
+        y|Y|yes|YES|Yes )
+          echo "Continiuing Installation!"
+        ;;
+        * )
+          nano .env
+        ;;
+    esac
+else
+  echo "Non-interactive installation parameter set. Installation begins."
+fi
 
 echo " "
 tput setaf 3;
@@ -32,12 +50,12 @@ echo "#########################################################################"
 
 echo " "
 tput setaf 4;
-echo "Installing reconPoint and it's dependencies"
+echo "Installing reNgine and it's dependencies"
 
 echo " "
 if [ "$EUID" -ne 0 ]
   then
-  tput setaf 1; echo "Error installing reconPoint, Please run this script as root!"
+  tput setaf 1; echo "Error installing reNgine, Please run this script as root!"
   tput setaf 1; echo "Example: sudo ./install.sh"
   exit
 fi
@@ -66,19 +84,21 @@ else
   tput setaf 2; echo "Docker installed!!!"
 fi
 
+
 echo " "
 tput setaf 4;
 echo "#########################################################################"
-echo "Installing docker compose"
+echo "Installing Docker Compose"
 echo "#########################################################################"
 if [ -x "$(command -v docker compose)" ]; then
-  tput setaf 2; echo "docker compose already installed, skipping."
+  tput setaf 2; echo "Docker Compose already installed, skipping."
 else
-  curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-  tput setaf 2; echo "docker compose installed!!!"
+  tput setaf 2; echo "Docker Compose installed!!!"
 fi
+
 
 echo " "
 tput setaf 4;
@@ -106,12 +126,14 @@ else
   exit 1
 fi
 
+
+
 echo " "
 tput setaf 4;
 echo "#########################################################################"
-echo "Installing reconPoint"
+echo "Installing reNgine"
 echo "#########################################################################"
-make certs && make build && make up && tput setaf 2 && echo "reconPoint is installed!!!" && failed=0 || failed=1
+make certs && make build && make up && tput setaf 2 && echo "reNgine is installed!!!" && failed=0 || failed=1
 
 if [ "${failed}" -eq 0 ]; then
   sleep 3
@@ -121,9 +143,10 @@ if [ "${failed}" -eq 0 ]; then
   echo "#########################################################################"
   echo "Creating an account"
   echo "#########################################################################"
-  make username
+  make username isNonInteractive=$isNonInteractive
 
-  tput setaf 2 && printf "\n%s\n" "Thank you for installing reconPoint, happy recon!!"
+  tput setaf 2 && printf "\n%s\n" "Thank you for installing reNgine, happy recon!!"
+  echo "In case you have unapplied migrations (see above in red), run 'make migrate'"
 else
-  tput setaf 1 && printf "\n%s\n" "reconPoint installation failed!!"
+  tput setaf 1 && printf "\n%s\n" "reNgine installation failed!!"
 fi
