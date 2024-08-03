3d2e0f579897891b11bd27cd22b2b7d671587134
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-08-03 08:52:54 +0600

    Update make.bat
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- make.bat
+++ make.bat
@@ -2,11 +2,15 @@
 
 :: Credits: https://github.com/ninjhacks
 
-set COMPOSE_ALL_FILES=-f docker-compose.yml
+set COMPOSE_ALL_FILES  = -f docker-compose.yml
 set SERVICES           = db web proxy redis celery celery-beat ollama
 
 :: Check if 'docker compose' command is available
-set DOCKER_COMPOSE=docker-compose
+docker compose version >nul 2>&1
+if %errorlevel% == 0 (
+    set DOCKER_COMPOSE=docker compose
+) else (
+    set DOCKER_COMPOSE=docker-compose
 )
 
 
@@ -15,10 +19,7 @@ if "%1" == "certs" %DOCKER_COMPOSE% -f docker-compose.setup.yml run --rm certs
 :: Generate certificates.
 if "%1" == "setup" %DOCKER_COMPOSE% -f docker-compose.setup.yml run --rm certs
 :: Build and start all services.
-if "%1" == "up" (
-    %DOCKER_COMPOSE% %COMPOSE_ALL_FILES% up -d --build %SERVICES%
-    if %errorlevel% neq 0 exit /b %errorlevel%
-)
+if "%1" == "up" %DOCKER_COMPOSE% %COMPOSE_ALL_FILES% up -d --build %SERVICES%
 :: Build all services.
 if "%1" == "build" %DOCKER_COMPOSE% %COMPOSE_ALL_FILES% build %SERVICES%
 :: Generate Username (Use only after make up).
@@ -40,10 +41,6 @@ if "%1" == "logs" %DOCKER_COMPOSE% %COMPOSE_ALL_FILES% logs --follow --tail=1000
 :: Show all Docker images.
 if "%1" == "images" %DOCKER_COMPOSE% %COMPOSE_ALL_FILES% images %SERVICES%
 :: Remove containers and delete volume data.
-if "%1" == "prune" (
-    %DOCKER_COMPOSE% %COMPOSE_ALL_FILES% stop %SERVICES%
-    docker-compose %COMPOSE_ALL_FILES% rm -f %SERVICES%
-    docker volume prune -f
-)
+if "%1" == "prune" %DOCKER_COMPOSE% %COMPOSE_ALL_FILES% stop %SERVICES% & docker-compose %COMPOSE_ALL_FILES% rm -f %SERVICES% & docker volume prune -f
 :: Show this help.
 if "%1" == "help" @echo Make application Docker images and manage containers using Docker Compose files only for windows.
