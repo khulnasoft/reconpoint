957bd28a87315cfc62c7a3d678b8130a708876d2
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-09-06 18:27:17 +0600

    Release/2.2.0 (#62)
    * 🚚 reconpoint.wiki recon.khulnasoft.com (#58)
    
    * Create .deepsource.toml
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    
    * refactor: change methods not using its bound instance to staticmethods (#61)
    
    The method doesn't use its bound instance. Decorate this method with `@staticmethod` decorator, so that Python does not have to instantiate a bound method for every instance of this class thereby saving memory and computation. Read more about staticmethods [here](https://docs.python.org/3/library/functions.html#staticmethod).
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>
    Co-authored-by: KhulnaSoft bot <43526132+khulnasoft-bot@users.noreply.github.com>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- web/scanEngine/management/commands/loadcustomengines.py
+++ web/scanEngine/management/commands/loadcustomengines.py
@@ -5,7 +5,8 @@ from reconPoint.common_func import load_custom_scan_engines
 class Command(BaseCommand):
     help = 'Loads custom engines from YAMLs in custom_engines/ folder into database'
 
-    def handle(self, *args, **kwargs):
+    @staticmethod
+    def handle(*args, **kwargs):
         return load_custom_scan_engines('/usr/src/app/custom_engines')
 
 
